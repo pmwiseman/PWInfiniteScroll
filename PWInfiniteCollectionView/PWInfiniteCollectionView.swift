d09bade1aaec44516d81d7bf6ec7f47c87a9f9f3
Patrick Wiseman <patrickmwiseman@gmail.com>
2017-08-05 22:43:04 -0700

    moved logic to collection view

--- PWInfiniteCollectionView/PWInfiniteCollectionView.swift
+++ PWInfiniteCollectionView/PWInfiniteCollectionView.swift
@@ -13,27 +13,59 @@ protocol PWInfiniteCollectionViewDataSource {
     func cellForItemAt(collectionView: UICollectionView, indexPath: IndexPath, dataIndexPath: IndexPath) -> UICollectionViewCell
 }
 
+protocol PWInfiniteCollectionViewDelegate {
+    func currentPageIndexDidChange(with pageIndex: Int)
+}
+
 class PWInfiniteCollectionView: UICollectionView {
     var infiniteDataSource: PWInfiniteCollectionViewDataSource?
+    var infiniteDelegate: PWInfiniteCollectionViewDelegate?
+    
+    fileprivate var pageCount = 0
+    fileprivate let extraPageCount = 2
+    fileprivate var pageHeight = CGFloat(0)
+    fileprivate var pageWidth = CGFloat(0)
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
+        //paging settings
+        isPagingEnabled = true
+        pageHeight = frame.height
+        pageWidth = frame.width
+        
+        //scroll settings
+        showsHorizontalScrollIndicator = false
+        let layout = collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.scrollDirection = .horizontal
+        
         dataSource = self
+        delegate = self
     }
     
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        
+        //offset to first page
+        let firstPageOffsetPoint = CGPoint(x: pageWidth, y: 0)
+        setContentOffset(firstPageOffsetPoint, animated: false)
+    }
+    
+    fileprivate var numberOfPages: Int {
+        return pageCount + extraPageCount
+    }
 }
 
+// MARK: - Data Source
+
 extension PWInfiniteCollectionView: UICollectionViewDataSource {
-    
-    // Datasource
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let newNumberOfItems = infiniteDataSource?.numberOfItems(collectionView: self) ?? 0
-        print("INDEXPATH ITEMCOUNT: \(newNumberOfItems + 2)")
-        return newNumberOfItems + 2
+        pageCount = infiniteDataSource?.numberOfItems(collectionView: self) ?? 0
+        return pageCount + extraPageCount
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -44,10 +76,9 @@ extension PWInfiniteCollectionView: UICollectionViewDataSource {
     }
     
     func calculateNewIndexPath(with indexPath: IndexPath) -> IndexPath {
-        let totalNumberOfItems = infiniteDataSource?.numberOfItems(collectionView: self) ?? 0
         if indexPath.row == 0 {
-            return IndexPath(item: 5, section: 0)
-        } else if indexPath.row == 7 {
+            return IndexPath(item: pageCount - 1, section: 0)
+        } else if indexPath.row == numberOfPages - 1 {
             return IndexPath(item: 0, section: 0)
         } else {
             return IndexPath(item: indexPath.row - 1, section: 0)
@@ -55,12 +86,49 @@ extension PWInfiniteCollectionView: UICollectionViewDataSource {
     }
 }
 
-extension PWInfiniteCollectionView {
-//    override var dataSource: UICollectionViewDataSource? {
-//        didSet {
-//            if let dataSource = self.dataSource {
-//                self.dataSource = dataSource.isEqual(self) ? self : dataSource
-//            }
-//        }
-//    }
+// MARK: - Flow Layout
+
+extension PWInfiniteCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: pageWidth, height: pageHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return CGFloat(0)
+    }
 }
+
+// MARK: - Scroll View Delegate
+
+extension PWInfiniteCollectionView: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //left scroll
+        if scrollView.contentOffset.x <= frame.origin.x {
+            let lastColorFrame = CGRect(x: pageWidth*CGFloat(pageCount), y: 0, width: pageWidth, height: frame.height)
+            scrollRectToVisible(lastColorFrame, animated: false)
+        } else if scrollView.contentOffset.x >= (contentSize.width - pageWidth) {
+            let firstColorFrame = CGRect(x: pageWidth, y: 0, width: pageWidth, height: frame.height)
+            scrollRectToVisible(firstColorFrame, animated: false)
+        }
+        let newPageIndex = calculatePageIndex(withOffset: contentOffset.x)
+        infiniteDelegate?.currentPageIndexDidChange(with: newPageIndex)
+    }
+    
+    private func calculatePageIndex(withOffset offset:CGFloat) -> Int{
+        let pageNumber = Int(offset/pageWidth)
+        if pageNumber == 0 {
+            return numberOfPages - 1
+        } else if pageNumber == numberOfPages - 1 {
+            return 0
+        } else {
+            return pageNumber - 1
+        }
+    }
+}
+
--- PWInfiniteCollectionView/ViewController.swift
+++ PWInfiniteCollectionView/ViewController.swift
@@ -14,42 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var collectionView: PWInfiniteCollectionView!
     
     var colors = [UIColor.blue, UIColor.black, UIColor.yellow, UIColor.cyan, UIColor.red, UIColor.purple]
-    var originalCount = 0
-    var pageWidth = CGFloat(0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        originalCount = colors.count
-        pageControl.numberOfPages = originalCount
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        pageWidth = view.frame.size.width
-        let frame = CGRect(x: pageWidth, y: 0, width: pageWidth, height: 100)
-        collectionView.scrollRectToVisible(frame, animated: false)
-    }
-    
-    func setPageControl(withOffset offset: CGFloat) {
-        let index = Int(floor(offset/pageWidth))
-        if index == 0 {
-            pageControl.currentPage = originalCount - 1
-        } else if index == colors.count - 1 {
-            pageControl.currentPage = 0
-        } else {
-            pageControl.currentPage = index - 1
-        }
+        pageControl.numberOfPages = colors.count
+        collectionView.infiniteDataSource = self
+        collectionView.infiniteDelegate = self
     }
 }
 
 extension ViewController: PWInfiniteCollectionViewDataSource {
-    
     func numberOfItems(collectionView: UICollectionView) -> Int {
         return colors.count
     }
     
     func cellForItemAt(collectionView: UICollectionView, indexPath: IndexPath, dataIndexPath: IndexPath) -> UICollectionViewCell {
-        print("INDEXPATH: \(dataIndexPath)")
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         let color = colors[dataIndexPath.row]
         cell.backgroundColor = color
@@ -57,32 +36,8 @@ extension ViewController: PWInfiniteCollectionViewDataSource {
     }
 }
 
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.size.width, height: view.frame.size.height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
-                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return CGFloat(0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return CGFloat(0)
-    }
-}
-
-extension ViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        //left scroll
-        if scrollView.contentOffset.x <= collectionView.frame.origin.x {
-            let lastColorFrame = CGRect(x: pageWidth*CGFloat(originalCount), y: 0, width: pageWidth, height: 100)
-            collectionView.scrollRectToVisible(lastColorFrame, animated: false)
-        } else if scrollView.contentOffset.x >= (collectionView.contentSize.width - pageWidth) {
-            let firstColorFrame = CGRect(x: pageWidth, y: 0, width: pageWidth, height: 100)
-            collectionView.scrollRectToVisible(firstColorFrame, animated: false)
-        }
-        setPageControl(withOffset: scrollView.contentOffset.x)
+extension ViewController: PWInfiniteCollectionViewDelegate {
+    func currentPageIndexDidChange(with pageIndex: Int) {
+        pageControl.currentPage = pageIndex
     }
 }
